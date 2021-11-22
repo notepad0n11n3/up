e3d4013a6b7cfdd4a5851f2570597ad71a461281
vKalWin <local@host.com>
2021-11-21 20:36:18 -1000

    clean

--- codigoF/01_hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-/*      */
-int main() //* Funcion principal
-{
-  printf("panda8hat x89p\n");
-  return 0;  //* igual que en bashScripting, indica que el programa termino con exito :)
-}
