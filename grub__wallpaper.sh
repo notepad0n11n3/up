33e883bb5397a61e3372fb4a61e778125c132d4d
mK <local@host.exe>
2023-01-04 20:37:24 -0600

    .

--- grub__wallpaper.sh
+++ grub__wallpaper.sh
@@ -3,16 +3,16 @@
 mango=$(whoami)
 umu=$(echo $up)
 
-  # las imagenes para remplazar son .png      voy a probar con .jpg
+  # las imagenes para remplazar son .png      
 
  
 #BLOQUE 1  
          # El fondo con las opciones del grub cambio PERO... despu[es de escoger una opci[on al momento de cargar se llega a ver por unos segundos el anterior wallpaper
-sudo cp /home/${mango}/Desktop/0n11n3/up/8/wallhaven-01339g.jpg /boot/grub/themes/kali/grub-4x3.png
-sudo cp /home/${mango}/Desktop/0n11n3/up/8/wallhaven-01339g.jpg /boot/grub/themes/kali/grub-16x9.png
+sudo cp /home/${mango}/Desktop/0n11n3/up/8/wallhaven-01339g.png /boot/grub/themes/kali/grub-4x3.png
+sudo cp /home/${mango}/Desktop/0n11n3/up/8/wallhaven-01339g.png /boot/grub/themes/kali/grub-16x9.png
 
 
 #BLOQUE 2 
         # fondeDelGrub postSelecci[on fondoCargaPreSystem     <<== este no es tan importantePOR AHORA
-sudo cp ${umu}/8/wallhaven-01339g.jpg /usr/share/grub/themes/kali/grub-4x3.png
-sudo cp ${umu}/8/wallhaven-01339g.jpg /usr/share/grub/themes/kali/grub-16x9.png
+sudo cp ${umu}/8/wallhaven-01339g.png /usr/share/grub/themes/kali/grub-4x3.png
+sudo cp ${umu}/8/wallhaven-01339g.png /usr/share/grub/themes/kali/grub-16x9.png
