66c7363e866cdc955a91345734ec00edd8222b3d
kK <local@host.exe>
2024-03-02 16:31:35 -0500

    .

--- grub__wallpaper.sh
+++ grub__wallpaper.sh
@@ -8,8 +8,8 @@ umu=$(echo $up)
  
 #BLOQUE 1  
          # El fondo con las opciones del grub cambio PERO... despu[es de escoger una opci[on al momento de cargar se llega a ver por unos segundos el anterior wallpaper
-sudo cp /home/${mango}/Desktop/0n11n3/up/8/wallhaven-01339g.png /boot/grub/themes/kali/grub-4x3.png
-sudo cp /home/${mango}/Desktop/0n11n3/up/8/wallhaven-01339g.png /boot/grub/themes/kali/grub-16x9.png
+sudo cp /home/${mango}/.0n11n3/up/8/wallhaven-01339g.png /boot/grub/themes/kali/grub-4x3.png
+sudo cp /home/${mango}/.0n11n3/up/8/wallhaven-01339g.png /boot/grub/themes/kali/grub-16x9.png
 
 
 #BLOQUE 2 
