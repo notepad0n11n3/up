4ded0d95c567004594e6aeb1722dad73027d9f68
KK <local@host.com>
2026-06-16 10:58:35 -0500

    .

---
+++ vm_slave_autoConf.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// TODO::  el .i3status.conf y ./config   se copian manualmente para no romper configuraciones en i3blocks
+
+int main(int argc, char **argv){
+
+	fprintf(stdout, "\xA\x9 Downloading 0n11n3 files for linux environments \xA");
+
+	const char *urls[]={"https://raw.githubusercontent.com/notepad0n11n3/up/refs/heads/master/zshrc_self",\
+											"https://raw.githubusercontent.com/notepad0n11n3/up/refs/heads/master/arch/i3/.i3status.conf",\
+											"https://raw.githubusercontent.com/notepad0n11n3/up/refs/heads/master/arch/i3/config",\
+											"https://raw.githubusercontent.com/notepad0n11n3/up/refs/heads/master/vimrc__nanorc/.vimrcESSENTIAL",\
+											"https://raw.githubusercontent.com/notepad0n11n3/up/refs/heads/master/vimrc__nanorc/nvim/init.lua",\
+											"https://raw.githubusercontent.com/notepad0n11n3/up/refs/heads/master/vimrc__nanorc/alacritty/alacritty.toml",\
+											"https://raw.githubusercontent.com/notepad0n11n3/up/refs/heads/master/8/wallhaven-01339g.png",\
+											"https://raw.githubusercontent.com/notepad0n11n3/up/refs/heads/master/8/terminal_Finale.jpg",\
+											"https://raw.githubusercontent.com/notepad0n11n3/up/refs/heads/master/8/biosho_bigDaddy_littleSstr.jpg",\
+											"https://raw.githubusercontent.com/notepad0n11n3/up/refs/heads/master/tmux/.tmux.conf"};
+
+	const char *paths={"bash -c \"mkdir -p ~/.config/{nvim,alacritty,i3} ~/.0n11n3/{up/8,vimrc__nanorc/{nvim,alacritty},tmux} \""};
+	const char *zshrc_tmux={"mv ./zshrc_self ~/.zshrc_self && mv ./.tmux.conf ~/.tmux.conf && echo 'source ~/.zshrc_self' >> ~/.bashrc && echo 'source ~/.zshrc_self' >> ~/.zshrc"};
+	const char *img_nvim_alac={"mv ./*.jpg ~/.0n11n3/up/8/ && mv ./*.png ~/.0n11n3/up/8/ && mv ./.vimrcESSENTIAL ~/.vimrc && mv ./alacritty.toml ~/.config/alacritty/ && mv ./init.lua ~/.config/nvim/"};
+	system(paths);
+
+
+	char mochimango[2048];
+	int number_urls = sizeof(urls) / sizeof(urls[0]);
+	fprintf(stdout, "\xA\x9\x9 Number urls: %d \xA", number_urls);
+	for (int i=0; i<number_urls; i++){
+		printf("\xA== %d == \xA", i);
+		fprintf(stdout, "\xA\x9\x9\x9 %d >> %s\xA", i, urls[i]);
+		snprintf(mochimango, sizeof(mochimango), "curl -O \"%s\"", urls[i]);
+		system(mochimango);
+	}
+	system(zshrc_tmux);
+	system(img_nvim_alac);
+
+	return 0;
+}
