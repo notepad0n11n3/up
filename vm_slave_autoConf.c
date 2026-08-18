b3ea3260fb0733a699aa82f3feaaa82789944d5d
KK <local@host.com>
2026-08-18 17:08:40 -0500

    .

--- vm_slave_autoConf.c
+++ vm_slave_autoConf.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 
 //	::: sha256sum ./vm_slave_autoConf
-//  d2545904b2e1dbab08ce6f0b95fe9341776fdde0373b1bacab7d9675b074a751 ./vm_slave_autoConf
+//	3006304894a4d2e6e63940ab4920f11aa90652c8be1a9ef1ce5802a760002012  ./vm_slave_autoConf
+
 
 // TODO::  el .i3status.conf y ./config   se copian manualmente para no romper configuraciones en i3blocks
 
@@ -37,14 +38,14 @@ int main(int argc, char **argv){
 	const char *gpg_files={"gpg --yes -dq -o ~/.zshrc_self --pinentry-mode loopback ./zshrc_self && gpg --yes -dq -o ~/.0n11n3/up/0x/lab_x89p --pinentry-mode loopback ./lab_x89p ; rm ./zshrc_self ./lab_x89p && echo '\xA\x9 Comprobando los sources\xA' && for i in ~/.bashrc ~/.zshrc; do grep -Hn 'source ~/.zshrc_self' ${i} || echo 'source ~/.zshrc_self' >> ${i}; done "};
 
 	// i3wm configFiles , copia solo si no existe, si existe muestra el diff (config , .i3status.conf)
-	const char *i3wm_config={"bash -c \"( if [ ! -f ~/.config/i3/config ]; then mv ./config ~/.config/i3/ ; else diff ./config ~/.config/i3/config ; rm ./config ;fi) && ( if [ ! -f ~/.config/i3/.i3status.conf ]; then mv ./.i3status.conf ~/.config/i3/ ; else diff ./.i3status.conf ~/.config/i3/.i3status.conf ; rm ./.i3status.conf ;fi)\""};
+	const char *i3wm_config={"bash -c \"( if [ ! -f ~/.config/i3/config ]; then mv ./config ~/.config/i3/ ; else echo -e '\xA\x9 .../i3/config\xA' && diff ./config ~/.config/i3/config ; rm ./config ;fi) && ( if [ ! -f ~/.config/i3/.i3status.conf ]; then mv ./.i3status.conf ~/.config/i3/ ; else echo -e '\xA\x9 .../i3/.i3status.conf\xA' && diff ./.i3status.conf ~/.config/i3/.i3status.conf ; rm ./.i3status.conf ;fi)\""};
 
 	const char *grub_img={"bash ./grub__wallpaper_noSUDO.sh && rm ./grub__wallpaper_noSUDO.sh"};
 	// if [ -f ./file_here ];then ....
 	// if test -f ./file_here;then ....							" La sintaxis con [] es un alias de  test  "
 
-	system(paths); // Creacion de paths
-	fprintf(stdout, "\xA\x9 ===>* Paths Ready... <3 <3 <3\xA\xA");
+	// Creacion de paths
+	(system(paths) == 0)? fprintf(stdout, "\xA\x9 \033[92m ===>* Paths Ready [OK]... <3 \033[0m \xA\xA") : fprintf(stderr, "\xA\x9 \033[92m ===>* Paths [FAIL]... \033[0m \xA\xA");
 
 	// Curl config files
 	char mochimango[4096];
@@ -57,15 +58,14 @@ int main(int argc, char **argv){
 		system(mochimango);
 	}
 
-	system(mv_config_files); // mv config files after curl
-	fprintf(stdout, "\xA\xA\x9 ===>* mv_configFiles Ready... <3 <3 <3\xA");
-	system(gpg_files);
-	fprintf(stdout, "\xA\x9 ===>* gpg Files Ready... <3 <3 <3\xA");
-	system(i3wm_config);
-	fprintf(stdout, "\xA\x9 ===>* i3wm config  if  Ready... <3 <3 <3\xA");
+	// mv config files after curl
+	(system(mv_config_files) == 0)? fprintf(stdout, "\xA\xA\x9 \033[92m ===>* mv_configFiles [OK]... <3 \033[0m\xA") : fprintf(stderr, "\xA\xA\x9 \033[91m ===>* mv_configFiles [FAIL]... \033[0m\xA") ;
 
-	system(grub_img);
-	fprintf(stdout, "\xA\x9 ===>* grub Ready... <3 <3 <3\xA");
+	(system(gpg_files) == 0)? fprintf(stdout, "\xA\x9 \033[92m ===>* gpg Files [OK]... <3 \033[0m\xA") : fprintf(stderr, "\xA\xA\x9 \033[91m ===>* gpg Files [FAIL]... \033[0m\xA");
+
+	(system(i3wm_config) == 0)? fprintf(stdout, "\xA\x9 \033[92m ===>* i3wm config if [OK]... <3 \033[0m\xA") : fprintf(stderr, "\xA\xA\x9 \033[91m ===>* i3wm config if [FAIL]... \033[0m\xA");
+
+	(system(grub_img) == 0)? fprintf(stdout, "\xA\x9 \033[92m ===>* grub [OK]... <3 \033[0m\xA") : fprintf(stderr, "\xA\xA\x9 \033[91m ===>* grub [FAIL]... \033[0m\xA");
 
 	return 0;
 }
